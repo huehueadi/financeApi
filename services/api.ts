f0e62f2f16aac9b5f0e1814f8d23cda48a472fa1
zotomation <zototech1@gmail.com>
2025-03-05 20:01:53 +0530

    first commit

--- services/api.ts
+++ services/api.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Create axios instance with base URL
 export const api = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://finance-server-q9i6rz2av-adiis-projects-48c442af.vercel.app/api',
   headers: {
     'Content-Type': 'application/json',
   },
